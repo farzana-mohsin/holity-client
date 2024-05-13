591ef8e1d88785a45218b7893a90daf2d789d5ed
Farzana Mohsin <fmohsin@usertesting.com>
2024-05-13 15:11:16 -0400

    add DELETE code for volunteer posts and complete functionality

--- src/Pages/ManageMyPosts/MyVolunteerPosts.jsx
+++ src/Pages/ManageMyPosts/MyVolunteerPosts.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
+import Swal from "sweetalert2";
 
 const MyVolunteerPosts = () => {
   const { user } = useContext(AuthContext);
@@ -15,10 +16,48 @@ const MyVolunteerPosts = () => {
       });
   }, [user]);
 
+  const handleDelete = (_id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`${import.meta.env.VITE_API_URL}/post/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your coffee has been deleted.",
+                icon: "success",
+              });
+
+              const remainingPosts = volunteerPosts.filter(
+                (request) => request._id !== _id
+              );
+              setVolunteerPosts(remainingPosts);
+            }
+          });
+      }
+      // if the user confirms they want to delete it, only then the data will be fetched
+    });
+  };
+
   return (
     <div>
       {volunteerPosts.map((post, index) => (
-        <p key={index}>{post.postTitle}</p>
+        <>
+          <p key={index}>{post.postTitle}</p>
+          <button>Update</button>
+          <button onClick={() => handleDelete(post._id)}>Delete</button>
+        </>
       ))}
     </div>
   );
--- src/Pages/ManageMyPosts/MyVolunteerRequests.jsx
+++ src/Pages/ManageMyPosts/MyVolunteerRequests.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
+import Swal from "sweetalert2";
 
 const MyVolunteerRequests = () => {
   const { user } = useContext(AuthContext);
@@ -16,10 +17,47 @@ const MyVolunteerRequests = () => {
       });
   }, [user]);
 
+  const handleDelete = (_id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`${import.meta.env.VITE_API_URL}/applications/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your coffee has been deleted.",
+                icon: "success",
+              });
+
+              const remainingRequests = volunteerRequests.filter(
+                (request) => request._id !== _id
+              );
+              setVolunteerRequests(remainingRequests);
+            }
+          });
+      }
+      // if the user confirms they want to delete it, only then the data will be fetched
+    });
+  };
+
   return (
-    <div>
+    <div className='container mx-auto flex '>
       {volunteerRequests.map((post, index) => (
-        <p key={index}>{post.category}</p>
+        <div key={index}>
+          <h1>{post.category}</h1>
+          <button onClick={() => handleDelete(post._id)}>Cancel</button>
+        </div>
       ))}
     </div>
   );
