6a071224ad4f12a42609bbc50b5f0d8143f6a95d
Farzana Mohsin <fmohsin@usertesting.com>
2024-05-13 22:09:20 -0400

    add no items found page for need volunteers page

--- src/Pages/NeedVolunteers/NeedVolunteers.jsx
+++ src/Pages/NeedVolunteers/NeedVolunteers.jsx
@@ -35,7 +35,7 @@ const NeedVolunteers = () => {
   };
 
   return (
-    <div>
+    <div className='h-full'>
       <Helmet>
         <title>Holity Social Support | Volunteers Needed</title>
       </Helmet>
@@ -68,38 +68,27 @@ const NeedVolunteers = () => {
       </form>
 
       {allItems.length ? (
-        "code below"
+        <>
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 container mx-auto gap-12 my-10 '>
+            {allItems.map((item, index) => (
+              <VolunteerCard
+                key={index}
+                item={item}
+              ></VolunteerCard>
+            ))}
+          </div>
+        </>
       ) : (
         <>
-          <section className='bg-white dark:bg-gray-900 '>
+          <section className='bg-white dark:bg-gray-900 h-[calc(83vh-300px)]'>
             <div className='container flex items-center px-6 py-12 mx-auto'>
               <div className='flex flex-col items-center max-w-sm mx-auto text-center'>
-                <p className='p-3 text-sm font-medium text-blue-500 rounded-full bg-blue-50 dark:bg-gray-800'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                    strokeWidth='2'
-                    stroke='currentColor'
-                    className='w-6 h-6'
-                  >
-                    <path
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                      d='M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z'
-                    />
-                  </svg>
-                </p>
-                <h1 className='mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl'>
-                  Page not found
-                </h1>
-                <p className='mt-4 text-gray-500 dark:text-gray-400'>
-                  The page you are looking for doesn&apos;t exist. Here are some
-                  helpful links:
+                <p className='mt-5 text-gray-500 dark:text-gray-400 text-2xl'>
+                  Sorry, the title you are looking for doesn&apos;t exist.
                 </p>
 
                 <div className='flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto'>
-                  <button className='w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600'>
+                  <button className='w-1/2 px-5 py-2  tracking-wide  transition-colors duration-200  shrink-0 sm:w-auto dark:hover:bg-blue-500 dark:bg-blue-600 btn bg-[#6faf9f] text-white lg:px-4 lg:py-2  text-sm rounded-xl lg:ml-2 hover:bg-[#727C82] border-2 border-[#f77d5c]'>
                     Take me home
                   </button>
                 </div>
@@ -108,15 +97,6 @@ const NeedVolunteers = () => {
           </section>
         </>
       )}
-
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 container mx-auto gap-12 my-10'>
-        {allItems.map((item, index) => (
-          <VolunteerCard
-            key={index}
-            item={item}
-          ></VolunteerCard>
-        ))}
-      </div>
     </div>
   );
 };
