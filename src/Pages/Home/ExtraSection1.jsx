98fe70897bde3bdf28c5d39ad58d6fd4bf8b61f5
Farzana Mohsin <fmohsin@usertesting.com>
2024-05-15 12:47:31 -0400

    change animation to volunteer cards

--- src/Pages/Home/ExtraSection1.jsx
+++ src/Pages/Home/ExtraSection1.jsx
@@ -31,28 +31,28 @@ const ExtraSection1 = () => {
       <div className='container mx-auto grid grid-cols-1 md:grid-cols-3 text-center mt-10 space-y-6 lg:space-y-0'>
         <div className=' '>
           <motion.h1 className='extraSection'>{rounded1}</motion.h1>
-          <h2 className='text-white lg:text-3xl font-bold  mb-2'>
+          <h2 className='text-white lg:text-[#ffe400] lg:text-3xl font-bold  mb-2'>
             Numbers of Volunteers
           </h2>
-          <p className='w-full text-[#ffe400] lg:text-xl mb-7'>
+          <p className='w-full text-[#ffe400] lg:text-white lg:text-xl mb-7'>
             We are ten thousand volunteers.
           </p>
         </div>
         <div className='w-full mx-auto'>
           <motion.h1 className='extraSection'>{rounded2}</motion.h1>
-          <h2 className='text-white lg:text-3xl font-bold  mb-2'>
+          <h2 className='text-white lg:text-[#ffe400] lg:text-3xl font-bold  mb-2'>
             Numbers of Projects
           </h2>
-          <p className='w-full text-[#ffe400] lg:text-xl mb-7'>
+          <p className='w-full text-[#ffe400] lg:text-white lg:text-xl mb-7'>
             Our primary objective is philanthropy.
           </p>
         </div>
         <div className='w-full'>
           <motion.h1 className='extraSection'>{rounded3}</motion.h1>
-          <h2 className='text-white lg:text-3xl font-bold mb-2'>
+          <h2 className='text-white lg:text-[#ffe400] lg:text-3xl font-bold mb-2'>
             Numbers of Volunteer Hours
           </h2>
-          <p className='text-[#ffe400] lg:text-xl mb-7'>
+          <p className='text-[#ffe400] lg:text-white lg:text-xl mb-7'>
             Volunteers are a valuable resource to support the planet.
           </p>
         </div>
