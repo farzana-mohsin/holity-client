3bb55148bdc33315b4d7b446a0faa990a671fe81
Farzana Mohsin <fmohsin@usertesting.com>
2024-05-15 10:25:44 -0400

    change  style for manage my posts page

--- src/Pages/ManageMyPosts/ManageMyPosts.jsx
+++ src/Pages/ManageMyPosts/ManageMyPosts.jsx
@@ -13,13 +13,15 @@ const ManageMyPosts = () => {
       <Tabs>
         <div className=' container px-6 py-10 mx-auto'>
           <h1 className='text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl '>
-            Browse Jobs By Categories
+            Browse Your Posts and Requests below!
           </h1>
 
           <p className='max-w-2xl mx-auto my-6 text-center text-gray-500 '>
-            Three categories available for the time being. They are Web
-            Development, Graphics Design and Digital Marketing. Browse them by
-            clicking on the tabs below.
+            Volunteering is a fundamental act of good citizenship. It’s also
+            essential in our society. We can also help you assess risk in
+            volunteer positions, effectively deal with related legal and ethical
+            issues, implement employer-supported volunteering policies in the
+            workplace—and more.
           </p>
           <div className='flex items-center justify-center'>
             <TabList className='react-tabs__tab'>
